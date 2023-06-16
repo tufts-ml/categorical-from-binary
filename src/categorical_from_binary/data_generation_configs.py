84e1cd255bc547bea120d6e5f36eb87120530666
mikewojnowicz <mwojnowi@uci.edu>
2023-06-16 14:30:23 -0600

    Don't recall what I was doing here.

--- src/categorical_from_binary/data_generation_configs.py
+++ src/categorical_from_binary/data_generation_configs.py
@@ -30,15 +30,18 @@ def make_data_generation_configs(
     multipliers_on_n_categories_to_create_n_covariates: List[int],
     multipliers_on_n_parameters_to_create_n_samples: List[int],
     list_of_scales_for_predictive_categories: List[float],
-    seed: int = 1,
+    seeds: int = 1,
 ) -> List[DataGenerationConfig]:
     """
     Uses the provided arguments to create the info needed to generate categorical regression
-    datasets; namey, (K,M,N,ssq_high), where K is the number of categories, M is the number of covariates,
-    N is the number of samples, and ssq_high controls the categorical response predictability.
+    datasets; (K,M,N,ssq_high, seed), where K is the number of categories, M is the number of covariates,
+    N is the number of samples, ssq_high controls the categorical response predictability, and seed is a seed.
 
     Then takes the Cartesian product of these things to create a list of Data Generation Configs.
     """
+    if isinstance(seeds, int):
+        seeds = [seeds]
+
     data_generation_configs = []
     for K in list_of_n_categories:
         for (
@@ -51,6 +54,9 @@ def make_data_generation_configs(
             ) in multipliers_on_n_parameters_to_create_n_samples:
                 N = P * multiplier_to_create_n_samples
                 for scale in list_of_scales_for_predictive_categories:
-                    data_generation_config = DataGenerationConfig(seed, K, M, N, scale)
-                    data_generation_configs.append(data_generation_config)
+                    for seed in list(seeds):
+                        data_generation_config = DataGenerationConfig(
+                            seed, K, M, N, scale
+                        )
+                        data_generation_configs.append(data_generation_config)
     return data_generation_configs
